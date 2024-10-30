7a3ff0935d9efebe4dc0e380a9a282f660b2a4b6
Raphael Saldanha <rfsaldanha@gmail.com>
2024-10-30 13:34:18 +0100

    Point to Digital Ocean S3

--- rclone_job.sh
+++ rclone_job.sh
@@ -20,40 +20,40 @@ echo -e "\n" >> rclone_datasus_log.txt
 # Mirror datasus FTP
 ## SIM
 echo -e "SIM files\n" >> rclone_datasus_log.txt
-rclone sync :ftp:dissemin/publicos/SIM datasus-ftp-mirror:s3datasus/SIM --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
+rclone sync :ftp:dissemin/publicos/SIM digitalocean:datasus-ftp-mirror/SIM --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
 
 ## SINASC
 echo -e "SINASC files\n" >> rclone_datasus_log.txt
-rclone sync :ftp:dissemin/publicos/SINASC datasus-ftp-mirror:s3datasus/SINASC --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
+rclone sync :ftp:dissemin/publicos/SINASC digitalocean:datasus-ftp-mirror/SINASC --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
 
 # SIH
 echo -e "SIH files\n" >> rclone_datasus_log.txt
-rclone sync :ftp:dissemin/publicos/SIHSUS datasus-ftp-mirror:s3datasus/SIHSUS --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
+rclone sync :ftp:dissemin/publicos/SIHSUS digitalocean:datasus-ftp-mirror/SIHSUS --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
 
 # SIA
 echo -e "SIA files\n" >> rclone_datasus_log.txt
-rclone sync :ftp:dissemin/publicos/SIASUS datasus-ftp-mirror:s3datasus/SIASUS --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
+rclone sync :ftp:dissemin/publicos/SIASUS digitalocean:datasus-ftp-mirror/SIASUS --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
 
 # SINAN
 echo -e "SINAN files\n" >> rclone_datasus_log.txt
-rclone sync :ftp:dissemin/publicos/SINAN datasus-ftp-mirror:s3datasus/SINAN --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
+rclone sync :ftp:dissemin/publicos/SINAN digitalocean:datasus-ftp-mirror/SINAN --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
 
 # CNES
 echo -e "CNES files\n" >> rclone_datasus_log.txt
-rclone sync :ftp:dissemin/publicos/CNES datasus-ftp-mirror:s3datasus/CNES --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
+rclone sync :ftp:dissemin/publicos/CNES digitalocean:datasus-ftp-mirror/CNES --ftp-host=ftp.datasus.gov.br --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --ftp-concurrency=5 --verbose --log-file=rclone_datasus_log.txt --exclude=*.{xml,csv}
 
 # Create tree file of mirror
-rclone tree datasus-ftp-mirror:s3datasus > rclone_datasus_files_tree.txt
-rclone tree datasus-ftp-mirror:s3datasus --dirs-only > rclone_datasus_dirs_tree.txt
+rclone tree digitalocean:datasus-ftp-mirror > rclone_datasus_files_tree.txt
+rclone tree digitalocean:datasus-ftp-mirror --dirs-only > rclone_datasus_dirs_tree.txt
 
 # Write end time
 echo -e "\n" >> rclone_datasus_log.txt
 date >> rclone_datasus_log.txt
 
 # Copy log and tree files to mirror
-rclone copy rclone_datasus_log.txt datasus-ftp-mirror:s3datasus
-rclone copy rclone_datasus_files_tree.txt datasus-ftp-mirror:s3datasus
-rclone copy rclone_datasus_dirs_tree.txt datasus-ftp-mirror:s3datasus
+rclone copy rclone_datasus_log.txt digitalocean:datasus-ftp-mirror
+rclone copy rclone_datasus_files_tree.txt digitalocean:datasus-ftp-mirror
+rclone copy rclone_datasus_dirs_tree.txt digitalocean:datasus-ftp-mirror
 
 # End timestamp
 echo -e "End of update\n" >> rclone_datasus_log.txt
